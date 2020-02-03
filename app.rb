82579ae032f949c16a35d76a555cf3de60602aca
ricardo39985 <ricardo39985@gmail.com>
2020-02-03 16:00:16 -0500

    Done.

--- app.rb
+++ app.rb
@@ -10,4 +10,28 @@ class App < Sinatra::Base
     (params[:number].to_i**2).to_s
   end
 
+  get '/say/:number/:phrase' do
+    "#{params[:phrase]*params[:number].to_i}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    case params[:operation]
+    when 'add'
+      "#{params[:number1].to_i + params[:number2].to_i}"
+    when 'subtract'
+      "#{params[:number1].to_i - params[:number2].to_i}"
+    when 'multiply'
+      "#{params[:number1].to_i * params[:number2].to_i}"
+    when 'divide'
+      "#{params[:number1].to_i / params[:number2].to_i}"
+    else
+      
+    end
+    
+  end
+
 end
